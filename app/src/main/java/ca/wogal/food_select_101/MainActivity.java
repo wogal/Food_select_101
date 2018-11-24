34f9b420b11bf87b69df3a943c0f39c3ec757f6c
wogal <egilgeous@gmail.com>
2018-11-24 13:52:37 -0500

    Branch 169 // working project ,,

--- app/src/main/java/ca/wogal/food_select_101/MainActivity.java
+++ app/src/main/java/ca/wogal/food_select_101/MainActivity.java
@@ -1,12 +1,13 @@
 package ca.wogal.food_select_101;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int earle = 0;
+
+    public int earle = 6634;
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary( "native-lib" );
@@ -16,11 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        earle++;
+        earle = 334;
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById( R.id.sample_text );
-        tv.setText( stringFromJNI() );
+     //   TextView tv = (TextView) findViewById( R.id.sample_text );
+      //  tv.setText( stringFromJNI() );
+        Toast.makeText( this, " wogal ", Toast.LENGTH_SHORT ).show();
+
     }
 
     /**
